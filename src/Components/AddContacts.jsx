a27bbb5151d6f9acc931b2eb2cbaff4869c57f1f
Bunyameen <bunyameen@gmail.com>
2022-12-31 15:54:32 +0100

    Contacts: WIP

---
+++ src/Components/AddContacts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AddContacts = () => {
+  return (
+    <div>
+        <h2>Add Contacts</h2>
+    </div>
+  )
+}
---
+++ src/Components/ContactsList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ContactsList = () => {
+  return (
+    <div>ContactsList / Form group</div>
+  )
+}
