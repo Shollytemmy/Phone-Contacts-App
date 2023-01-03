d79d17f90958a0631a35a5e9a59f1c95893e99d0
Bunyameen <bunyameen@gmail.com>
2023-01-03 08:29:53 +0100

    Contacts: React-Router-Dom added

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// importing React-Router from 'react-router
+import {BrowserRouter} from 'react-router-dom'
 import App from './App'
 import './index.css'
 
@@ -8,7 +10,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
   
-    <App />
+   
   
 )
